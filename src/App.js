570f2f2b82b159e0dcfd205219c3d16646a55886
Anirudh Mahajan <anirudhmahajan18@gmail.com>
2019-08-30 16:19:41 +0530

    Did code clean up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+// Main Application Component
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
+// Exporting the main Application Component
 export default App;
